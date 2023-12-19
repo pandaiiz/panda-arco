8a435ba4e62bc5209834c134f7ad427a039373a6
ht <610502604@qq.com>
2023-12-19 18:32:16 +0800

    feat: 修复收发记录bug

--- src/pages/produce-hub/records/constants.tsx
+++ src/pages/produce-hub/records/constants.tsx
@@ -1,47 +1,56 @@
 import React from 'react';
-import { Button, Popconfirm } from '@arco-design/web-react';
+import { Button, Popconfirm, TableColumnProps, Tag } from '@arco-design/web-react';
 
 export function getColumns(
   callback: (record: Record<string, any>, type: string) => Promise<void>
-) {
+): TableColumnProps[] {
   return [
     {
       title: '流程单号',
       dataIndex: 'transferId',
+      width: 100,
+      align: 'center',
     },
     {
       title: '部门',
       dataIndex: 'department.name',
+      align: 'center',
     },
     {
       title: '类型',
       dataIndex: 'productTypeName',
+      align: 'center',
     },
     {
       title: '出入库',
       dataIndex: 'type',
-      render: (value) => value === 'IN' ? '入库' : '出库'
+      align: 'center',
+      render: (value) => value === 'IN' ? <Tag color='green'> 入库</Tag> : <Tag color='blue'> 出库</Tag>
     },
     {
       title: '重量',
       dataIndex: 'weight',
+      align: 'center',
     },
     {
       title: '件数',
       dataIndex: 'quantity',
+      align: 'center',
     },
     {
       title: '备注',
       dataIndex: 'remark',
+      align: 'center',
     },
     {
       title: '品名',
       dataIndex: 'transfer.style.categoryName',
+      align: 'center',
     },
     {
       title: '操作',
       dataIndex: 'operations',
-      headerCellStyle: { paddingLeft: '15px' },
+      align: 'center',
       render: (_, record) => [
         <Button
           type="text"
